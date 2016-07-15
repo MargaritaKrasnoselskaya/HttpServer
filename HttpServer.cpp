a36ad4e760e1daab637c1676de0bd9397dcd87ec
MargaritaKrasnoselskaya <margaritakrasnoselskaya@gmail.com>
2016-07-16 03:32:38 +0400

    Update HttpServer.cpp
    deleted one empty string

--- HttpServer.cpp
+++ HttpServer.cpp
@@ -120,20 +120,20 @@ void ProcessRequest(int sock)
         path = m_dir+"/"+path;//full file name
         if (stat (path.c_str(), &buffer) != 0)
         {   bodycontent = "There were no such file or resource. But We can tell you a fairytale. Once upon a time...";
-            GetHeader("404 File Not Found\nContent-length: 0\n", header);
+            GetHeader("404 File Not Found\nContent-length: 0", header);
             break;
         }
         if (ReadFile(path, bodycontent)==0)
         {
             bodycontent = "There were no such file or resource. But We can tell you a fairytale. Once upon a time...";
-            GetHeader("404 File Not Found\nContent-length: 0\n", header);
+            GetHeader("404 File Not Found\nContent-length: 0", header);
             break;
         }
         else
         {
             //we already have bodycontent
             string szstr = to_string(bodycontent.size());
-            string tmp = "200 OK\nContent-length: "+szstr+"\n";
+            string tmp = "200 OK\nContent-length: "+szstr;
             GetHeader(tmp, header);
         }
     } while (false);
